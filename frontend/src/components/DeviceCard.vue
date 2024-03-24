6b668901ec774419aee5f303f86636bb7d734cd8
Yil Verdeja <yilverdeja@gmail.com>
2024-03-24 21:05:37 +0800

    fix: colors to device page

--- frontend/src/components/DeviceCard.vue
+++ frontend/src/components/DeviceCard.vue
@@ -1,27 +1,45 @@
 <script setup lang="ts">
-import { AnchorLink, Card, Divider, Row, Col } from 'ant-design-vue';
+import { AnchorLink, Card, Row, Col } from 'ant-design-vue';
 import { DeviceResponse } from '../types';
+import { computed } from 'vue';
 
-defineProps<{
+const props = defineProps<{
 	device: DeviceResponse;
 	loading: boolean;
 	onSelect: () => void;
 }>();
+
+const roundToOneDecimal = (value: number): number => {
+	return Math.round(value * 10) / 10;
+};
+
+const calculateCarbonValue = (
+	value: number
+): { value: number; units: string } => {
+	if (value > 1000) {
+		return { value: roundToOneDecimal(value / 1000), units: 'Tonnes' };
+	} else {
+		return { value: roundToOneDecimal(value), units: 'Kgs' };
+	}
+};
+
+const carbonItem = computed(() => {
+	return calculateCarbonValue(props.device.totalCarbon || 0);
+});
 </script>
 
 <template>
 	<AnchorLink @click="onSelect">
 		<Card :title="device.name" :loading="loading" :hoverable="true">
 			<div v-if="device.totalCarbon && device.totalDiesel">
-				<Divider>Energy Savings</Divider>
 				<Row :gutter="16">
 					<Col class="gutter-row" :span="12">
-						<p>Total Carbon</p>
-						<p>{{ device.totalCarbon }}</p>
+						<p style="font-weight: bold">Total Carbon</p>
+						<p>{{ carbonItem.value }} {{ carbonItem.units }}</p>
 					</Col>
 					<Col class="gutter-row" :span="12">
-						<p>Total Diesel</p>
-						<p>{{ device.totalDiesel }}</p>
+						<p style="font-weight: bold">Total Diesel</p>
+						<p>{{ roundToOneDecimal(device.totalDiesel) }} Litre</p>
 					</Col>
 				</Row>
 			</div>
@@ -29,4 +47,9 @@ defineProps<{
 	</AnchorLink>
 </template>
 
-<style scoped></style>
+<style>
+.ant-card .ant-card-head {
+	color: darkcyan;
+	background-color: lightcyan;
+}
+</style>
